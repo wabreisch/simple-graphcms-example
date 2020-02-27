5d98df864f89aa2c775c3ba70145ba8eddd780f7
Wes Breisch <breisch@live.com>
2020-02-27 15:23:14 -0500

    add basic UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import styled from "styled-components";
+
+import "./App.css";
+
+const GET_RECIPES = gql`
+  {
+    recipes {
+      id
+      title
+      prepTime
+      ingredients {
+        id
+        name
+        description
+      }
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_RECIPES);
+
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>GraphCMS Example</h1>
+
+        {data.recipes.map(recipe => (
+          <RecipeLink href={`recipe/${recipe.id}`} key={recipe.id}>
+            {recipe.title}
+          </RecipeLink>
+        ))}
       </header>
     </div>
   );
 }
 
 export default App;
+
+const RecipeLink = styled.a`
+  margin: 0.3em 0;
+  color: #7f7;
+  text-decoration-style: dashed;
+`;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+
+const GRAPHCMS_URL = "https://api-useast.graphcms.com/v1";
+const GRAPHCMS_APP_ID = "ck73jiuqv0f0501cyclst5roz";
+const GRAPHCMS_API = `${GRAPHCMS_URL}/${GRAPHCMS_APP_ID}/master`;
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: GRAPHCMS_API }),
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
